615fca8ff9f701789a2e7ace5fded6ce779cd8ef
Hasnain150 <90840161+Hasnain150@users.noreply.github.com>
2023-09-06 19:37:36 +0500

    Add files via upload

---
+++ index.js
@@ -0,0 +1,32 @@
+// https://api.qrserver.com/v1/create-qr-code/?size=150x150&data=Example
+
+const btn=document.querySelector(".app button");
+const inputBarValue=document.querySelector("#inputBar");
+const imgDiv=document.querySelector(".img");
+
+const qrCode=()=>{
+   imgDiv.innerHTML=""
+    let inputt=inputBarValue.value;
+    if(!(inputt=="")){
+        imgDiv.style.display="inherit";
+        let link=`https://api.qrserver.com/v1/create-qr-code/?size=150x150&data=${inputt}`;
+        let img=document.createElement("img");
+        img.setAttribute("src",link);
+        img.setAttribute("alt","Internet Not Working");
+        imgDiv.appendChild(img);
+    }else {
+        imgDiv.style.display="none";
+        inputBarValue.classList.toggle("error");
+        setTimeout(() => {
+            inputBarValue.classList.toggle("error");
+        }, 1000);
+    }
+
+   }
+ 
+btn.addEventListener("click",qrCode)
+inputBarValue.addEventListener("keydown",(e)=>{
+    if(e.key==="Enter"){
+        qrCode();
+    }
+})
